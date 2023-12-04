1d22ba6ad25bd0851500c073ad8ee76cbedf617d
sorgaz12 <sergioorgaz02@gmail.com>
2023-12-04 13:58:32 +0100

    Inicio Proyecto

---
+++ src/UsoArrays.java
@@ -0,0 +1,11 @@
+
+public class UsoArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Arrays miArray=new Arrays(5);
+		miArray.getDatosArray();
+
+	}
+}
